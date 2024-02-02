647dd265fa9444668d2184c7c4bad18c70a264d0
senaditya <aditya.sen1hl@gmail.com>
2024-02-03 02:23:37 +0430

    added api

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from flask import Flask, render_template
 import requests
-from dotenv import load_dotenv
-import os
+
+# from dotenv import load_dotenv
+# import os
 
 # import json
 
-load_dotenv()
-apikey = os.getenv("NEWS_API")
+# load_dotenv()
+# apikey = os.getenv("NEWS_API")
+
+apikey = "1038c4e42381ce597f16ce3dd1d21039"
 
 app = Flask(__name__)
 
