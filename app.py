ede033fc37b721a5b36668c503de8d5388b7bdfe
senaditya <aditya.sen1hl@gmail.com>
2024-02-03 02:17:11 +0430

    init

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+import requests
+from dotenv import load_dotenv
+import os
+
+# import json
+
+load_dotenv()
+apikey = os.getenv("NEWS_API")
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    query = "latest"
+    max = "12"
+    fixed = f"&lang=en&country=in&max={max}&apikey={apikey}"
+    url = f"https://gnews.io/api/v4/search?q={query}{fixed}"
+    response = requests.get(url)
+    # News data (replace this with your actual data)
+    news_data = response.json()  # this is after combining the news.property
+    # with open("./sample.json", "r") as openfile:
+    #     # Reading from json file
+    #     json_object = json.load(openfile)
+    # news_data = json_object["articles"]
+    # print(news_data)
+    return render_template("index.html", news_data=news_data["articles"])
+
+
+if __name__ == "__main__":
+    app.run()
